b9a63dd77357725c643f3c642b9b89467c0c97d2
Gokuldev <gokuldevse2001@gmail.com>
2025-03-05 11:16:19 +0530

    fixed error

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/encryplto-lock/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
